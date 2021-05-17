2b1f97e574bdd44d93bd31bc80d8f660c693e2f0
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2021-05-17 18:34:06 +0700

    Redirect calls are now published as events

--- services/redirection/src/handlers/redirect.py
+++ services/redirection/src/handlers/redirect.py
@@ -1,16 +1,61 @@
 import os
 import json
+import logging
+import datetime as dt
 
 from pynamodb.exceptions import DoesNotExist
+import boto3
 
 from src.models import ShortenedLink
 from src.exceptions import ExternalError, InternalError
 
 
+client = boto3.client('events')
+
+logger = logging.getLogger(__name__)
+if os.environ['STAGE'] == 'dev':
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+
 def handler(event, context):
+    logger.debug(f'Processing event: {event}')
     try:
         link_id = event['pathParameters']['linkId']
+        if not link_id:
+            return {
+                'statusCode': 302,
+                'headers': {
+                    'Location': 'https://shortrlink.com/'
+                }
+            }
         link = ShortenedLink.get(link_id, 'LINK')
+
+        # Make known that someone actually used the service
+        res = client.put_events(
+            Entries=[
+                {
+                    'Time': dt.datetime.now(),
+                    'Source': 'aws.lambda',
+                    'Resources': [
+                        context.invoked_function_arn
+                    ],
+                    'DetailType': 'shortrLink user redirect',
+                    'Detail': json.dumps({
+                        'link_id': link_id,
+                        'ip': event['requestContext']['sourceIp'],
+                        'user_agent': event['requestContext']['userAgent'],
+                        'origin': event['headers'].get('Origin', ''),
+                        'headers': event['headers']
+                    }),
+                    'EventBusName': os.environ['EVENT_BUS_NAME'],
+                    'TraceHeader': event['headers']['x-amzn-trace-id']
+                },
+            ]
+        )
+        if res['FailedEntryCount']:
+            logger.exception(f'Failed publishing click/redirect event.')
 
         return {
             'statusCode': 302,
