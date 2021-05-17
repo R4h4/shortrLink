baedd41ded9727f605446782c4af0b20a17ca05e
R4h4 <karsten.eckhardt@gmail.com>
2021-05-17 22:54:58 +0700

    Every click now updates the counter on the main table

--- services/api/src/handlers/index_redirect_dynamodb.py
+++ services/api/src/handlers/index_redirect_dynamodb.py
@@ -2,7 +2,15 @@ import os
 import json
 import logging
 import datetime as dt
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
 from src.models import ShortenedLink
+
+
+# apply the XRay handler to all clients.
+patch_all()
 
 
 logger = logging.getLogger(__name__)
@@ -13,5 +21,11 @@ else:
 
 
 def handler(event, context):
-    """Increment the click count on the DynamoDb item for the Link by 1"""
+    """
+    Increment the click count on the DynamoDb item for the Link by 1
+    In theory, we could run into race conditions, but at a point where multiple clicks within ~10ms occur,
+    approximations should be fine
+    """
     logger.debug(f'Processing event: {event}')
+    link = ShortenedLink.get(event['detail']['link_id'], 'LINK')
+    link.update(actions=[ShortenedLink.clicks.set(link.clicks + 1)])
