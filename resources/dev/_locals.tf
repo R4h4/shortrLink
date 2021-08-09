b5141b02157be884da8d9d349085b4c1b591ea3f
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2021-08-09 14:49:25 +0700

    Added cross-regional eventbridge busses

---
+++ resources/dev/_locals.tf
@@ -0,0 +1,11 @@
+locals {
+  stage = "dev"
+  app = "shortrLink"
+
+  default_tags = {
+    terraform = "true"
+    project = local.app,
+    service = "main"
+    stage = local.stage
+  }
+}
--- resources/dev/main.tf
+++ resources/dev/main.tf
@@ -1,8 +1,3 @@
-locals {
-  stage = "dev"
-  app = "shortrLink"
-}
-
 terraform {
   required_providers {
     aws = {
@@ -16,19 +11,6 @@ terraform {
     key = "state/startdust/sender_service.dev.tfstate"
     region = "eu-west-1"
     profile = "privateGmail"
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-  profile = "privateGmail"
-  default_tags {
-    tags = {
-      terraform = "true"
-      project = local.app,
-      service = "main"
-      stage = local.stage
-    }
   }
 }
 
@@ -151,6 +133,91 @@ module "cognito_user_pool" {
 resource "aws_cloudwatch_event_bus" "main" {
   name = format("%s_%s_bus", local.app, local.stage)
 }
+
+data "aws_iam_policy_document" "aws_cloudwatch_event_bus_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["events:PutEvents"]
+    resources = [aws_cloudwatch_event_bus.main.arn]
+  }
+}
+
+resource "aws_iam_role" "eventbridge_events" {
+  name = "eventbridge_events"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "publish_events_to_eventbridge" {
+  policy = data.aws_iam_policy_document.aws_cloudwatch_event_bus_policy.json
+  role = aws_iam_role.eventbridge_events.id
+}
+
+resource "aws_cloudwatch_event_bus" "ap_southeast_1" {
+  name = format("%s_%s_bus", local.app, local.stage)
+  provider = aws.ap-southeast-1
+}
+
+resource "aws_cloudwatch_event_bus" "us_east_1" {
+  name = format("%s_%s_bus", local.app, local.stage)
+  provider = aws.us-east-1
+}
+
+resource "aws_cloudwatch_event_rule" "duplicate_events_us_east_1_rule" {
+  name = "duplicate-events-for-cross-region"
+  description = "Duplicates all events from us-east-1 into the primary region"
+  event_bus_name = aws_cloudwatch_event_bus.us_east_1.name
+  event_pattern = <<PATTERN
+    {
+      "source": [{"prefix": "" }]
+    }
+  PATTERN
+
+  provider = aws.us-east-1
+}
+
+resource "aws_cloudwatch_event_target" "duplicate_events_us_east_1_target" {
+  arn = aws_cloudwatch_event_bus.main.arn
+  rule = aws_cloudwatch_event_rule.duplicate_events_us_east_1_rule.name
+  event_bus_name = aws_cloudwatch_event_bus.us_east_1.name
+  role_arn = aws_iam_role.eventbridge_events.arn
+
+  provider = aws.us-east-1
+}
+
+resource "aws_cloudwatch_event_rule" "duplicate_events_ap_southeast_1_rule" {
+  name = "duplicate-events-for-cross-region"
+  description = "Duplicates all events FROM ap-southeast-1 into the primary region"
+  event_bus_name = aws_cloudwatch_event_bus.ap_southeast_1.name
+  event_pattern = <<PATTERN
+    {
+      "source": [{"prefix": "" }]
+    }
+  PATTERN
+
+  provider = aws.ap-southeast-1
+}
+
+resource "aws_cloudwatch_event_target" "duplicate_events_ap_southeast_1_target" {
+  arn = aws_cloudwatch_event_bus.main.arn
+  rule = aws_cloudwatch_event_rule.duplicate_events_ap_southeast_1_rule.name
+  event_bus_name = aws_cloudwatch_event_bus.ap_southeast_1.name
+  role_arn = aws_iam_role.eventbridge_events.arn
+
+  provider = aws.ap-southeast-1
+}
+
 
 resource "aws_ssm_parameter" "user_pool_id" {
   name = "/${local.app}/${local.stage}/user_pool_id"
