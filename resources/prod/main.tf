e0d6cc2424d7f03822eb6b138c92193b1f98b510
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2021-08-13 10:45:13 +0700

    Added table name ssm parameters to secondary regions

--- resources/prod/main.tf
+++ resources/prod/main.tf
@@ -179,6 +179,20 @@ resource "aws_ssm_parameter" "table_name" {
   value = module.dynamodb_table.table_name
 }
 
+resource "aws_ssm_parameter" "table_name_us_east_1" {
+  name = format("/%s/%s/dynamodb_table_name", local.app,local.stage)
+  type = "String"
+  value = module.dynamodb_table.table_name
+  provider = aws.us-east-1
+}
+
+resource "aws_ssm_parameter" "table_name_ap_southeast_1" {
+  name = format("/%s/%s/dynamodb_table_name", local.app,local.stage)
+  type = "String"
+  value = module.dynamodb_table.table_name
+  provider = aws.ap-southeast-1
+}
+
 resource "aws_ssm_parameter" "dynamodb_stream_arn" {
   name = format("/%s/%s/dynamodb_stream_arn", local.app,local.stage)
   type = "String"
